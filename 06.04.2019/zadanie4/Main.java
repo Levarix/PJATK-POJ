08f6323aeaed85aed5476a601d2168fbe19b7c91
Maciej Rybacki <34714430+Levarix@users.noreply.github.com>
2019-04-25 19:25:22 +0200

    Create Main.java

---
+++ 06.04.2019/zadanie4/Main.java
@@ -0,0 +1,12 @@
+/*
+POJ - laboratorium 3 2019-04-06
+Grupa: Maciej Rybacki i Jakub Słomiński
+Zadanie 4
+*/
+public class Main {
+
+    public static void main(String[] args) {
+       Date date1 = new Date (5, 3, 2016);
+       System.out.println(date1.toString());
+    }
+}
