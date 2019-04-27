3d3d924e4c0ebfbe5ec54661107402189dd8c558
Maciej Rybacki <34714430+Levarix@users.noreply.github.com>
2019-04-27 09:18:31 +0200

    Create zad7.java

---
+++ 06.04.2019/zad7.java
@@ -0,0 +1,22 @@
+public class Palindrom {
+
+    private String palindrom;
+
+    public void ustawPalindrom(String palindrom) {
+        palindrom = palindrom.replace(" ", "");
+        palindrom = palindrom.replace(",", "");
+        palindrom = palindrom.replace(".", "");
+        palindrom = palindrom.toLowerCase();
+        this.palindrom = palindrom;
+    }
+
+
+    public boolean sprawdzPalindrom() {
+        for (int i = 0; i <= palindrom.length() / 2; i++) {
+            if (palindrom.charAt(i) != palindrom.charAt(palindrom.length() - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
