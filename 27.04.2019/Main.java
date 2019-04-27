abc87a71ca0b86e59092f6c27047075c1b312a96
Maciej Rybacki <rybackimaciej03@gmail.com>
2019-04-27 11:24:06 +0200

    Created Main and Person class

---
+++ 27.04.2019/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Person p1 = new Person("Iwona", "Sezamkowa 16");
+        Person p2 = new Person("Maciej", "Fiołkowa 3/3");
+
+        System.out.println(p1.toString());
+        System.out.println(p2.toString());
+    }
+}
