b3ef0d4d56543bc03b159227b76a24827cf1c04c
Maciej Rybacki <rybackimaciej03@gmail.com>
2019-05-08 19:41:16 +0200

    Added 04_03

---
+++ 27.04.2019/04_03/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Point p1 = new Point(3.4f, 3.6f);
+        Point p2 = new Point(4.4f,4.5f);
+
+        System.out.println(p1.toString());
+        System.out.println(p2.toString());
+
+        MovablePoint mp1 = new MovablePoint(3.5f, 3.5f, 1.0f, 1.0f);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(mp1.move());
+        }
+    }
+}
+
