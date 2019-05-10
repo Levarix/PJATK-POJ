281260891656c9bfddf59fca482ac9fd56b70b5a
Maciej Rybacki <rybackimaciej03@gmail.com>
2019-05-10 20:57:15 +0200

    Added 04_05

---
+++ 27.04.2019/04_05/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Armstrong number1 = new Armstrong(153);
+        Armstrong number2 = new Armstrong(123);
+
+        System.out.println(number1.Armstrong());
+        System.out.println(number2.Armstrong());
+    }
+}
+
