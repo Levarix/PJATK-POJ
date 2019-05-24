6dc1b02983a0cc5c17730f0719f91ecf4ad9c892
Maciej Rybacki <rybackimaciej03@gmail.com>
2019-05-24 21:47:39 +0200

    Added new exc

--- 11.05.2019/05_02/MyCircle.java
+++ 11.05.2019/05_02/MyCircle.java
@@ -1,78 +1,62 @@
-import java.lang.reflect.Array;
-
+// Lukasz Rachwal S19638
+// Maciej Rybacki S18553
 public class MyCircle {
-    private MyPoint center = new MyPoint(0,0);
+    private  MyPoint center =new MyPoint(0,0);
     private int radius = 1;
+    private int centerX;
+    private int centerY;
+    private int centerXY[] = new int[2];
+    ;
 
-    public MyCircle() {
+    public MyCircle(){
 
     }
-
-    public MyCircle (int x, int y, int radius) {
-        this.x = x;
-        this.y = y;
-        this.radius = radius;
+    public MyCircle(MyPoint center, int radius){
+        this.center=center;
+        this.radius=radius;
     }
-
-    public MyCircle(MyPoint center, int radius) {
+    public MyPoint getCenter() {
+        return center;
+    }
+    public void setCenter(MyPoint center) {
         this.center = center;
-        this.radius = radius;
     }
 
     public int getRadius() {
         return radius;
     }
-
     public void setRadius(int radius) {
         this.radius = radius;
     }
-
-    public MyPoint getCenter() {
-        return center;
+    public int getCenterX(){
+        return centerX;
     }
-
-    public void setCenter(MyPoint center) {
-        this.center = center;
+    public void setCenterX(int centerX){
+        this.centerX=centerX;
     }
-
-    public int getCenterX() {
-        return x;
+    public int getCenterY(){
+        return centerY;
     }
-
-    public int setCenterX(int x) {
-        this.x = x;
+    public void setCenterY(int centerY){
+        this.centerY = centerY;
     }
-
-    public int getCenterY() {
-        return y;
+    public int[] getCenterXY(){
+        return centerXY;
     }
-
-    public int setCenterY(int y) {
-        this.y = y;
-    }
-
-    public int[] getCenterXY() {
-        int[] xy = {x, y};
-        return xy;
-    }
-    public void setCenterXY(int x, int y) {
-        this.x = x;
-        this.y = y;
+    public void setCenterXY(int centerXY[]){
+        this.centerXY = centerXY;
     }
     public String toString() {
-        return "MyCircle[radius="+radius+", center=("MyPoint.toString()+"]";
+        return "MyCircle{" + "center=" + center + ", radius=" + radius +'}';
     }
-
-    public double getArea() {
-
-    }
-
-    public double getCirumference() {
+    public double getArea(){
+        return Math.PI * Math.pow(getRadius(), 2);
 
     }
-
-    public double distance(MyCircle antoher) {
-
+    public double getCircumference() {
+        return 2 * Math.PI * getRadius();
     }
+
 }
-
+~
+~
