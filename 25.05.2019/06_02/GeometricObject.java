623e50cff022afed52bd08a90f1433a85052be76
artofbw <artofbw@gmail.com>
2019-05-25 11:41:30 +0200

    excersise 06_02

---
+++ 25.05.2019/06_02/GeometricObject.java
@@ -0,0 +1,8 @@
+// praca w parach: Łukasz Ćwikliński s19334, Maciej Rybacki s18553
+
+package 04_02;
+
+public interface GeometricObject {
+    public double getPerimeter();
+    public double getArea();
+}
---
+++ 25.05.2019/06_02/Resizable.java
@@ -0,0 +1,7 @@
+// praca w parach: Łukasz Ćwikliński s19334, Maciej Rybacki s18553
+
+package 04_02;
+
+public interface Resizable {
+    public void resize(int percent);
+}
